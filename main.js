3287817de65bd1498bf4da79a36ef23b1ddc50fa
Verstarker <hayden.wrathall@gmail.com>
2018-06-07 13:06:32 +1200

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+$('document').ready(function ($) {
+
+    $('#HomeScroll').click(function() {
+        $('html, body').animate({
+            scrollTop: 0 
+        }, 500);
+    })
+
+    $('#AboutScroll').click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top - 150
+        }, 500);
+    })
+
+    $('#ProjectsScroll').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top - 53
+        }, 500);
+    })
+
+    $('#ContactScroll').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top - 53
+        }, 500);
+    })
+
+});
